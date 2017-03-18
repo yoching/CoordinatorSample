423645a1fb0c92041257e316913907c3e043734f
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2017-03-18 16:29:05 +0900

    Add project

---
+++ CoordinatorSample/CoordinatorSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoordinatorSample
+//
+//  Created by Yoshikuni Kato on 3/18/17.
+//  Copyright © 2017 Yoshikuni Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
